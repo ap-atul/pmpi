eeb295f869de29e974a801f0f0e408547c0dc84c
AP-Atul <atulpatare99@gmail.com>
2021-01-15 23:01:01 +0530

    added setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pmpi',
+    version='0.0.1',
+    author='AP-Atul',
+    author_email='atulpatare99@gmail.com',
+    packages=['pmpi/'],
+    url='https://github.com/AP-Atul/pmpi',
+    license='LICENSE',
+    description='Python Message Passing Interface using Pipe communication',
+    long_description=open('README.md').read(),
+)
